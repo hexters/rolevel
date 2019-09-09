a1c3e24ab57b95fdd46e678bad00daf5778d42b6
Saifullah <hexters@gmail.com>
2019-09-09 09:01:53 +0700

    Update 0000_00_00_000000_create_roles_table.php

--- src/Migrations/0000_00_00_000000_create_roles_table.php
+++ src/Migrations/0000_00_00_000000_create_roles_table.php
@@ -19,7 +19,7 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->string('slug')->unique();
             $table->longText('permissions')->nullable();
             $table->string('state')->nullable();
